fef15a2c2049efc1801b4c3050fc9c20ec6ae893
Ethan Ramos <ethan.n.ramos@gmail.com>
2025-04-24 15:33:07 -0400

    Created function headers for remaining routes

--- server/routes/drivers.py
+++ server/routes/drivers.py
@@ -5,7 +5,7 @@ import sqlite3
 drivers_bp = Blueprint('drivers', __name__)
 
 
-@drivers_bp.route('/')
+@drivers_bp.route('/', methods=['GET'])
 def get_all_drivers():
     conn, cursor = create_connection()
     if not validate_connection(conn):
@@ -40,7 +40,7 @@ def get_all_drivers():
         conn.close()
 
 
-@drivers_bp.route('/<driver_id>')
+@drivers_bp.route('/<driver_id>', method=['GET'])
 def get_driver_by_id(driver_id):
     conn, cursor = create_connection()
     if not validate_connection(conn):
@@ -73,3 +73,78 @@ def get_driver_by_id(driver_id):
         }), 500
     finally:
         conn.close()
+
+
+@drivers_bp.route('/search', methods=['GET'])
+def search_drivers():
+    pass
+
+
+@drivers_bp.route('/filter', methods=['GET'])
+def filter_drivers():
+    pass
+
+
+@drivers_bp.route('/<driver_id>/stats', methods=['GET'])
+def get_driver_stats(driver_id):
+    pass
+
+
+@drivers_bp.route('/<driver_id>/seasons', methods=['GET'])
+def get_driver_seasons(driver_id):
+    pass
+
+
+@drivers_bp.route('/<driver_id>/teams', methods=['GET'])
+def get_driver_teams(driver_id):
+    pass
+
+
+@drivers_bp.route('/compare', methods=['GET'])
+def compare_drivers():
+    pass
+
+
+@drivers_bp.route('/standings', methods=['GET'])
+def get_driver_standings():
+    pass
+
+
+@drivers_bp.route('/<driver_id>/current-team', methods=['GET'])
+def get_driver_current_team(driver_id):
+    pass
+
+
+@drivers_bp.route('/<driver_id>/seasons/<year>', methods=['GET'])
+def get_driver_season(driver_id, year):
+    pass
+
+
+@drivers_bp.route('/<driver_id>/races', methods=['GET'])
+def get_driver_races(driver_id):
+    pass
+
+
+@drivers_bp.route('/<driver_id>/podiums', methods=['GET'])
+def get_driver_podiums(driver_id):
+    pass
+
+
+@drivers_bp.route('/<driver_id>/first-win', methods=['GET'])
+def get_driver_first_win(driver_id):
+    pass
+
+
+@drivers_bp.route('/<driver_id>/championships', methods=['GET'])
+def get_driver_championships(driver_id):
+    pass
+
+
+@drivers_bp.route('/<driver_id>/vs/<other_driver_id>', methods=['GET'])
+def compare_head_to_head(driver_id, other_driver_id):
+    pass
+
+
+@drivers_bp.route('/<driver_id>/teammates', methods=['GET'])
+def get_driver_teammates(driver_id):
+    pass
