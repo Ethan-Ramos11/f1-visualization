d081fc9af9182ff30c9cfe30ea2f8f52e8478518
Ethan Ramos <ethan.n.ramos@gmail.com>
2025-04-24 15:23:55 -0400

    Created the get_all_drivers route

--- server/routes/drivers.py
+++ server/routes/drivers.py
@@ -1,9 +1,8 @@
 from flask import Blueprint, jsonify
-from database.create_tables import create_connection
+from database.create_tables import create_connection, validate_connection
 import sqlite3
 
 drivers_bp = Blueprint('drivers', __name__)
-
 
 
 @drivers_bp.route('/')
@@ -11,20 +10,31 @@ def get_all_drivers():
     conn, cursor = create_connection()
     if not validate_connection(conn):
         return jsonify({
-            'status': 'error',
+            'status': 'Error',
             'message': 'Database connection failed',
             'data': None
         }), 500
-    query = "SELECT * FROM drivers"
-    cursor.execute(query)
-    drivers = cursor.fetchall()
-    if drivers:
+    try:
+        query = "SELECT * FROM drivers"
+        cursor.execute(query)
+        drivers = cursor.fetchall()
+        if drivers:
+            return jsonify({
+                'status': 'success',
+                'message': 'Drivers found',
+                'data': drivers
+            }), 200
+        else:
+            return jsonify({
+                'status': 'Error',
+                'message': 'Drivers not found',
+                'data': []
+            }), 404
+    except sqlite3.Error as e:
         return jsonify({
-            'status': 'success',
-            'message': 'Drivers found',
-            'data': drivers
-        }), 200
-    else:
-        return jsonify({
-
-        }), 404
+            'status': 'error',
+            'message': f'Database error: {e}',
+            'data': None
+        }), 500
+    finally:
+        conn.close()
